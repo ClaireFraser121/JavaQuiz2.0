69a99678a7782ec483b0309e3118beebfb6644b0
Claire Fraser <clairefraser121@gmail.com>
2023-11-07 15:26:14 +0000

    Global variables

---
+++ assets/javaScript/logic.js
@@ -0,0 +1,12 @@
+// Define your quiz logic here
+// Global variables
+let currentQuestionIndex = 0;
+let timeLeft = 0;
+let score = 0;
+
+// DOM elements
+const startButton = document.getElementById('start');
+const questionTitle = document.getElementById('question-title');
+const choicesContainer = document.getElementById('choices');
+const feedbackElement = document.getElementById('feedback');
+
